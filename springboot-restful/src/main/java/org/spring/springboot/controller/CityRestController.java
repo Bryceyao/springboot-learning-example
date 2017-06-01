43a84efc98f7aa20f7b76b311e681c5772ef3363
Bryceyao <bryce.yao@wolaidai.com>
2017-06-01 17:37:08 +0800

    1

--- springboot-restful/src/main/java/org/spring/springboot/controller/CityRestController.java
+++ springboot-restful/src/main/java/org/spring/springboot/controller/CityRestController.java
@@ -18,27 +18,27 @@ public class CityRestController {
     @Autowired
     private CityService cityService;
 
-    @RequestMapping(value = "/api/city/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/api/fcity/{id}", method = RequestMethod.GET)
     public City findOneCity(@PathVariable("id") Long id) {
         return cityService.findCityById(id);
     }
 
-    @RequestMapping(value = "/api/city", method = RequestMethod.GET)
+    @RequestMapping(value = "/api/lcity", method = RequestMethod.GET)
     public List<City> findAllCity() {
         return cityService.findAllCity();
     }
 
-    @RequestMapping(value = "/api/city", method = RequestMethod.POST)
+    @RequestMapping(value = "/api/ccity", method = RequestMethod.POST)
     public void createCity(@RequestBody City city) {
         cityService.saveCity(city);
     }
 
-    @RequestMapping(value = "/api/city", method = RequestMethod.PUT)
+    @RequestMapping(value = "/api/mcity", method = RequestMethod.PUT)
     public void modifyCity(@RequestBody City city) {
         cityService.updateCity(city);
     }
 
-    @RequestMapping(value = "/api/city/{id}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/api/dcity/{id}", method = RequestMethod.DELETE)
     public void modifyCity(@PathVariable("id") Long id) {
         cityService.deleteCity(id);
     }
