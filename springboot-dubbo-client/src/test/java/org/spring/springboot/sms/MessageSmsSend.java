da7a5c3942fc47fa50f89735bd23130b25034b79
Bryceyao <bryce.yao@wolaidai.com>
2018-03-27 19:59:58 +0800

    1

---
+++ springboot-dubbo-client/src/test/java/org/spring/springboot/sms/MessageSmsSend.java
@@ -0,0 +1,5 @@
+package org.spring.springboot.sms;
+
+public class MessageSmsSend {
+
+}
--- springboot-mybatis-redis/src/main/java/org/spring/springboot/service/impl/CityServiceImpl.java
+++ springboot-mybatis-redis/src/main/java/org/spring/springboot/service/impl/CityServiceImpl.java
@@ -3,7 +3,7 @@ package org.spring.springboot.service.impl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spring.springboot.dao.CityDao;
-import org.spring.springboot.entity.City;
+import org.spring.springboot.domain.City;
 import org.spring.springboot.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
